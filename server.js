02acbf2656bba8297b85fcb285b98401e6b032ce
aasis59 <aasis452711@gmail.com>
2024-03-15 19:08:00 +0545

    added

--- server.js
+++ server.js
@@ -12,17 +12,28 @@ const posts = [
     title: "Post 2",
   },
 ];
+function authenticationToken(req,res,next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader?.split(' ')[1];
+    if(token == null) return res.sendStatus(401)
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECERT,(err, user)=>{
+if(err)return res.sendStatus(403)
+req.user = user
+next()
+})
+}
 app.use(express.json());
-
-app.get("/posts", (req, res) => {
-  res.json(posts);
+app.get("/posts", authenticationToken,(req, res) => {
+  
+    res.json(posts.filter(post => post.username === req.user.name));
 });
 app.post("/login", (req, res) => {
   const username = req.body.username;
   const user = { name: username };
    const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECERT);
-   res.json({accessToken:accessToken})
+   res.json({ token:accessToken})
 });
+
 app.listen(3000, () => {
   console.log("Wellcome to server.js");
 });
