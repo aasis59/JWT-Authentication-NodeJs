89aa2413fce75e41a0820c4d8d6f041313837f36
aasis59 <aasis452711@gmail.com>
2024-03-15 12:49:43 +0545

    added

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-console.log("hello");
-const express = require('express');
-const  jwt = require('jsonwebtoken')
+require('dotenv').config();
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const app = express();
-const posts =[
-    {
-        username:"kyle",
-        title:'Post 1'
-    },
-    {
-        username:"jim",
-        title:'Post 2'
-    }
+const posts = [
+  {
+    username: "kyle",
+    title: "Post 1",
+  },
+  {
+    username: "jim",
+    title: "Post 2",
+  },
+];
+app.use(express.json());
 
-]
-app.get('/posts',(req,res)=>{
-    res.json(posts);
-
-})
-app.get('/login',(req,res)=>{
-const username = req.body.username
-const user = {name: username}
-jwt.sign()
-})
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const user = { name: username };
+   const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECERT);
+   res.json({accessToken:accessToken})
+});
 app.listen(3000, () => {
-    console.log("Wellcome to server.js");
-})
+  console.log("Wellcome to server.js");
+});
